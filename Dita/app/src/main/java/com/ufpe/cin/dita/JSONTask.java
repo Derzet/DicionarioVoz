3984f2ca3165dafff1e93f78175baa897a982889
Matheus <mhc@cin.ufpe.br>
2016-11-01 16:12:50 -0300

    Adiciona a modularização

---
+++ Dita/app/src/main/java/com/ufpe/cin/dita/JSONTask.java
@@ -0,0 +1,96 @@
+package com.ufpe.cin.dita;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Matheus on 01/11/2016.
+ */
+
+public class JSONTask extends AsyncTask<String,String,String> {
+    protected String resultado;
+
+    @Override
+    protected String doInBackground(String... urls) {
+
+
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urls[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            InputStream stream = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                buffer.append(line);
+
+            }
+            String finalJSON = buffer.toString();
+
+            JSONObject parentObject0 = new  JSONObject(finalJSON);
+            JSONObject parentObject = parentObject0.getJSONObject("entry");
+            JSONArray parentArray = parentObject.getJSONArray("sense");
+            //parentObject.getJSONArray("sense");
+
+            StringBuffer bufferFinal = new StringBuffer();
+
+
+            JSONObject finalObject = parentArray.getJSONObject(0);
+            String id =  parentObject.getString("@id");
+            String gramGrp= " "+finalObject.getString("gramGrp");
+            String definition = " "+finalObject.getString("def");
+            bufferFinal.append(id);
+            bufferFinal.append(gramGrp);
+            bufferFinal.append(definition);
+            // id+" "+gramGrp+" "+definition
+            return bufferFinal.toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+
+            try {
+                if (reader != null) reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        //  textView.setText(result);
+        resultado = result;
+
+
+    }
+
+    public String getResultado(){
+
+        return resultado;
+    }
+}
--- Dita/app/src/main/java/com/ufpe/cin/dita/MainActivity.java
+++ Dita/app/src/main/java/com/ufpe/cin/dita/MainActivity.java
@@ -1,19 +1,10 @@
 package com.ufpe.cin.dita;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     protected TextView wordTextView;
@@ -30,68 +21,17 @@ public class MainActivity extends AppCompatActivity {
         buttonView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new JSONTask().execute("http://dicionario-aberto.net/search-json/arroz");
+
+              JSONTask jsontask = new JSONTask();
+                String site = "http://dicionario-aberto.net/search-json/";
+                site.concat("arroz");
+                jsontask.execute(site);
+                wordTextView.setText( jsontask.getResultado());
             }
         });
 
 
         };
-
-        public class JSONTask extends AsyncTask<String,String,String> {
-
-
-            @Override
-            protected String doInBackground(String... urls) {
-
-
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
-                try {
-                    URL url = new URL(urls[0]);
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.connect();
-                    InputStream stream = connection.getInputStream();
-                    reader = new BufferedReader(new InputStreamReader(stream));
-                    StringBuffer buffer = new StringBuffer();
-                    String line = "";
-                    while ((line = reader.readLine()) != null) {
-
-                        buffer.append(line);
-
-                    }
-                  String finalJSON = buffer.toString();
-                   // JSONObject finalObject = new JSONObject(finalJSON);
-
-
-
-
-                   return buffer.toString();
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (connection != null) {
-                        connection.disconnect();
-                    }
-
-                    try {
-                        if (reader != null) reader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(String result) {
-                super.onPostExecute(result);
-              //  textView.setText(result);
-                wordTextView.setText(result);
-            }
-        }
 
 
 
